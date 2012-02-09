cbc51a90bbc3ed69ae8fe6294011b95fa7afe581
umedoblock <devnull@localhost>
2012-02-10 03:59:40 +0900

    pure python で書いている Bar_base の indent を修正。

--- src/bar_ext/bar/__init__.py
+++ src/bar_ext/bar/__init__.py
@@ -19,36 +19,36 @@ class Bar_abstract(metaclass=ABCMeta):
     def abs_meth(self):
         pass
 
-class Bar_base(Bar_abstract):
-    CONST = 'PURE PYTHON'
-    def __init__(self, n=100):
-        print('Bar_base.__init__()')
-        print('Bar_base.__mro__ =', Bar_base.__mro__)
-        print('self.__dict__ =', self.__dict__)
-        print('id(self.__dict__) =', id(self.__dict__))
-        s = super()
-        print('super().__init__() in Bar_base pure Python')
-        print('s =', s)
-        print('s.__thisclass__ =', s.__thisclass__)
-        print('s.__self__ =', s.__self__)
-        print('s.__self_class__ =', s.__self_class__)
-        s.__init__(n)
-
-    def number(self):
-        return self.num
-
-    def string(self):
-        return self.ss
-
-    def abs_meth(self):
-        return 'abstractmethod'
-
 try:
     from _bar import Bar_base
     print('you use c extension')
 except ImportError as e:
     print('e =', e)
     print('you should get c extension')
+
+    class Bar_base(Bar_abstract):
+        CONST = 'PURE PYTHON'
+        def __init__(self, n=100):
+            print('Bar_base.__init__()')
+            print('Bar_base.__mro__ =', Bar_base.__mro__)
+            print('self.__dict__ =', self.__dict__)
+            print('id(self.__dict__) =', id(self.__dict__))
+            s = super()
+            print('super().__init__() in Bar_base pure Python')
+            print('s =', s)
+            print('s.__thisclass__ =', s.__thisclass__)
+            print('s.__self__ =', s.__self__)
+            print('s.__self_class__ =', s.__self_class__)
+            s.__init__(n)
+
+        def number(self):
+            return self.num
+
+        def string(self):
+            return self.ss
+
+        def abs_meth(self):
+            return 'abstractmethod'
 
 class Bar(Bar_base):
     __doc__ = 'Bar.__doc__'
