8423bedfc25eb3cb95000021c812d0628b98cda3
umedoblock <devnull@localhost>
2012-02-13 23:59:21 +0900

    p2.__dict__ を表示するようにした。
    (refs #14)

--- src/par2/p2_inherit.py
+++ src/par2/p2_inherit.py
@@ -6,7 +6,7 @@ print('p2.__sizeof__() =', p2.__sizeof__())
 # print('p2.__dict__ =', p2.__dict__)
 p2.v = 8
 print('p2.v =', p2.v)
-# print('p2.__dict__ =', p2.__dict__)
+print('p2.__dict__ =', p2.__dict__)
 print('id(p2.__dict__) = 0x{:08x}'.format(id(p2.__dict__)))
 print('p2.dump(p2) =')
 p2.dump(p2, p2.__sizeof__())
