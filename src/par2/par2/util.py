3023b8ebbb75b17150e11f013adf045fc7f717e7
umedoblock <devnull@localhost>
2012-02-19 10:18:25 +0900

    matrix_to_bytes(), bytes_to_matrix() の対応。
    3octets(24bit)単位でencode(), decode()できるようにする。
    (refs #32)

--- src/par2/par2/util.py
+++ src/par2/par2/util.py
@@ -4,16 +4,24 @@ __all__ = [ \
     'matrix_to_bytes', 'bytes_to_matrix'
 ]
 
-def matrix_to_bytes(matrix):
+def matrix_to_bytes(matrix, code_size):
     elements = [None] * len(matrix)
     for i, m in enumerate(matrix):
-        elements[i] = struct.pack('=' + 'H' * len(m), *m)
+      # elements[i] = struct.pack('=' + 'H' * len(m), *m)
+        ms = [int.to_bytes(mm, code_size, 'big') for mm in m]
+        elements[i] = b''.join(ms)
+#   print('elements =', elements)
     return b''.join(elements)
 
-def bytes_to_matrix(bys, redundancy, horizontal_size):
-    matrix = [None] * redundancy
-    for i in range(redundancy):
+def bytes_to_matrix(bys, element_num, horizontal_size, code_size):
+    matrix = [None] * element_num
+    for i in range(element_num):
         horizontal_bytes = bys[i*horizontal_size:(i+1)*horizontal_size]
-        vector = struct.unpack('=' + 'H' * redundancy, horizontal_bytes)
-        matrix[i] = list(vector)
+        nums = [None] * element_num
+        for j in range(element_num):
+            byts = horizontal_bytes[j*code_size:(j+1)*code_size]
+            num = int.from_bytes(byts, 'big')
+            nums[j] = num
+        matrix[i] = nums
+      # print('matrix[{}] = {}'.format(i, matrix[i]))
     return matrix
