e2cfb17ef99ce8d39ddcdf7f97a930ba251a638b
umedoblock <devnull@localhost>
2012-02-25 22:45:11 +0900

    argparseを使ってtest_speed.py実行時の動作を変更できるようにする。
    (refs #47)

--- src/par2/par2/test_speed.py
+++ src/par2/par2/test_speed.py
@@ -2,6 +2,7 @@
 import datetime
 import math
 import copy
+import argparse
 
 import unittest
 import sys, os
@@ -13,9 +14,25 @@ sys.path.insert(0, libdir)
 from par2 import Par2
 from par2 import Par2Archive
 
+parser = argparse.ArgumentParser(description='speed test for par2.')
+
+parser.add_argument('--megabytes', metavar='N', dest='mb',
+                   type=int, nargs='?', default=0,
+                   help='mega bytes (default: 0)')
+
+parser.add_argument('--kilobytes', metavar='N', dest='kb',
+                   type=int, nargs='?', default=256,
+                   help='kilo bytes (default: 256)')
+
+parser.add_argument('--redundancy', metavar='N', dest='redundancy',
+                   type=int, nargs='?', default=15,
+                   help='kilo bytes (default: 15)')
+
 def run():
-    MB = 0
-    KB = 256
+    args = parser.parse_args()
+    MB = args.mb
+    KB = args.kb
+    redundancy = args.redundancy
 
     mega = MB * 1024 ** 2
     kilo = KB * 1024 ** 1
@@ -31,35 +48,34 @@ def run():
     bitss = (24,)
     bitss = (8, 16, 24)
     for bits in bitss:
-        print('{} bits symbol {:f} MB memory encode/decode speed'.
-                format(bits, mb))
-        for redundancy in (15,):
-            archive = Par2Archive(bits, redundancy)
-            archive.take_data(data)
-            # make parities
-            s = datetime.datetime.now()
-            archive.make_parities()
-            e = datetime.datetime.now()
-            encode_seconds =  (e - s).total_seconds()
-            encode_speed = data_size / 1024 ** 2 / encode_seconds
-            print('encode {:.2f} seconds, speed {:.2f} MB/sec'.
-                format(encode_seconds, encode_speed))
+        print('{} bits symbol {} redundancy {:f} MB memory encode/decode speed'.
+                format(bits, redundancy, mb))
+        archive = Par2Archive(bits, redundancy)
+        archive.take_data(data)
+        # make parities
+        s = datetime.datetime.now()
+        archive.make_parities()
+        e = datetime.datetime.now()
+        encode_seconds =  (e - s).total_seconds()
+        encode_speed = data_size / 1024 ** 2 / encode_seconds
+        print('encode {:.2f} seconds, speed {:.2f} MB/sec'.
+            format(encode_seconds, encode_speed))
 
-            # delete all parts
-            archive.slots[:archive.par2.redundancy] = \
-                [None] * archive.par2.redundancy
+        # delete all parts
+        archive.slots[:archive.par2.redundancy] = \
+            [None] * archive.par2.redundancy
 
-            # fix up
-            s = datetime.datetime.now()
-            raw_data = archive.fix_up_data()
-            e = datetime.datetime.now()
-            decode_seconds =  (e - s).total_seconds()
-            decode_speed = data_size / 1024 ** 2 / decode_seconds
-            print('decode {:.2f} seconds, speed {:.2f} MB/sec'.
-                format(decode_seconds, decode_speed))
+        # fix up
+        s = datetime.datetime.now()
+        raw_data = archive.fix_up_data()
+        e = datetime.datetime.now()
+        decode_seconds =  (e - s).total_seconds()
+        decode_speed = data_size / 1024 ** 2 / decode_seconds
+        print('decode {:.2f} seconds, speed {:.2f} MB/sec'.
+            format(decode_seconds, decode_speed))
 
-            print('data == raw_data is', data == raw_data)
-            print()
+        print('data == raw_data is', data == raw_data)
+        print()
 
 if __name__ == '__main__':
     run()
