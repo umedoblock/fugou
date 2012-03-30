76a8682a8aeb76be653c21c5002b420e58253964
umedoblock <devnull@localhost>
2012-03-30 16:34:06 +0900

    --encode, --decode を指定できるようにした。
    (refs #45)

--- src/par2/par2/libpar2_sample.c
+++ src/par2/par2/libpar2_sample.c
@@ -1,5 +1,7 @@
 #include "libpar2.h"
 
+#define ENABLE 1
+
 typedef struct _opts_t {
     int encode;
     int decode;
@@ -10,14 +12,16 @@ void usage(void)
     fprintf(stderr, "libpar2_sample [filename or -]\n");
 }
 
-int invalid_args(int argc, char *argv[])
+int invalid_opts(opts_t *opts)
 {
     int invalid = 1;
 
-    if (argc == 2) {
+    if (opts->encode == ENABLE ||
+        opts->decode == ENABLE) {
         invalid = 0;
     }
 
+err:
     return invalid;
 }
 
@@ -45,9 +49,9 @@ int parse_arg(opts_t *opts, int argc, char *argv[])
     /* check encode or decode. */
     for (i=0;i<argc;i++) {
         if (strcmp("--encode", argv[i]) == 0)
-            opts->encode = 1;
-        else if (strcmp("--decode", argv[i]) == 0)
-            opts->decode = 1;
+            opts->encode = ENABLE;
+        if (strcmp("--decode", argv[i]) == 0)
+            opts->decode = ENABLE;
     }
 
     return 0;
@@ -59,14 +63,12 @@ int main(int argc, char *argv[])
     opts_t opts;
 
     parse_arg(&opts, argc, argv);
-    if (invalid_args(argc, argv)) {
+    if (invalid_opts(&opts)) {
         view_arg(argc, argv);
         usage();
         return -1;
     }
-    else {
-        view_opts_t(&opts);
-    }
+    view_opts_t(&opts);
 
     return 0;
 }
