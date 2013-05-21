f3deac8c7921704528a0cdb512c1fbd5f16f4de6
umedoblock <devnull@localhost>
2013-05-21 09:45:55 +0900

    CAMELLIA_NORMED_SIZE macro 用の test 書いた。 (refs #287)

---
+++ src/test_libfugou.c
@@ -0,0 +1,49 @@
+#include "libfugou.h"
+
+void _validate(int success, char *test_name)
+{
+    if (success) {
+        fprintf(stdout, ".");
+    }
+    else {
+        fprintf(stderr, "failed %s\n", test_name);
+    }
+}
+
+void test_camellia_normed_size(void)
+{
+    int success;
+    size_t i, text_size, encode_size;
+    char msg[BUFFER_SIZE];
+
+    for (i=0;i<=CAMELLIA_BLOCK_SIZE * 2;i++) {
+        success = 0;
+        text_size = i;
+        encode_size = CAMELLIA_NORMED_SIZE(text_size);
+        if (text_size < CAMELLIA_BLOCK_SIZE) {
+            success = (encode_size == CAMELLIA_BLOCK_SIZE);
+        }
+        else if (text_size >= CAMELLIA_BLOCK_SIZE &&
+                 text_size < CAMELLIA_BLOCK_SIZE * 2) {
+            success = (encode_size == 2 * CAMELLIA_BLOCK_SIZE);
+        }
+        else if (text_size = CAMELLIA_BLOCK_SIZE * 2) {
+            success = (encode_size == 3 * CAMELLIA_BLOCK_SIZE);
+        }
+        snprintf(msg, BUFFER_SIZE, "test_camellia_normed_size(), "
+                                   "text_size = %u, encode_size = %u",
+                                    text_size, encode_size);
+        _validate(success, msg);
+    }
+}
+
+void test(void)
+{
+    test_camellia_normed_size();
+}
+
+int main(void)
+{
+    test();
+    return 0;
+}
