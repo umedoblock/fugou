7bb5df17ede86164072dc13df3010d79c3cd1a71
umedoblock <devnull@localhost>
2014-01-09 23:45:11 +0900

    なんということでしょうか！ 高速化と思った方法は低速化方法だったのです！ 事前の検証が重要である所以であります。
    :!gcc ./reed_solomon/guard.c -o ./reed_solomon/guard && ./reed_solomon/guard
    result=0x00000000
    e-s=3090000
    result=0x00000000
    e-s=3360000
    
    :!gcc ./reed_solomon/guard.c -o ./reed_solomon/guard && ./reed_solomon/guard
    result=0x00000000
    e-s=3080000
    result=0x00000000
    e-s=3330000
    
    :!gcc -O3 ./reed_solomon/guard.c -o ./reed_solomon/guard && ./reed_solomon/guard
    
    result=0x00000000
    e-s=470000
    result=0x00000000
    e-s=1310000
    (refs #330)

--- src/reed_solomon/guard.c
+++ src/reed_solomon/guard.c
@@ -10,7 +10,7 @@ typedef unsigned int uint;
 int main(void)
 {
     /* no need to think about 32bit */
-    uint a32, b32, gfi32[7], result, gf_max = 7;
+    uint a32, b32, gfi32[7], result, gf_max = 7, guard;
     clock_t s, e, t0, t1;
     int i, j, k, n = 10000000;
 
@@ -32,6 +32,26 @@ int main(void)
                     /* a32 < b32 */
                     result ^= gfi32[gf_max + a32 - b32];
                 }
+            }
+        }
+    }
+    e = clock();
+    fprintf(stdout, "result=0x%08x\n", result);
+    fprintf(stdout, "e-s=%ld\n", e - s);
+
+    result = 0U;
+    s = clock();
+    for (k=0;k<n;k++) {
+        for (j=0;j<7;j++) {
+            for (i=0;i<7;i++) {
+                a32 = gfi32[i];
+                b32 = gfi32[j];
+                guard = 0U;
+
+                if (a32 < b32)
+                    guard = gf_max;
+
+                result ^= gfi32[guard + a32 - b32];
             }
         }
     }
