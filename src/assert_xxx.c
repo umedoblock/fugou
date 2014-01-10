c96bbf4f7fbb0569d95726bbd2e3732182605664
umedoblock <devnull@localhost>
2014-01-11 00:39:11 +0900

    refactoring. fail => [FAILED] (refs #330)

--- src/assert_xxx.c
+++ src/assert_xxx.c
@@ -28,7 +28,7 @@ void assert_by_00(uchar *result,
     }
 
     if (failed(success)) {
-        fprintf(_f, "failed %s\n", test_name);
+        fprintf(_f, "[FAILED] %s\n", test_name);
         dump(result, zero_size, 16);
         fprintf(_f, "expected 0x00 * %zu, but result[%zu] is 0x%02x.\n",
                      zero_size, i, result[i]);
@@ -44,7 +44,7 @@ void assert_by_mem(uchar *expected,
     int cmp = memcmp(expected, result, mem_size);
     int success = (cmp == 0);
     if (failed(success)) {
-        fprintf(_f, "failed %s\n", test_name);
+        fprintf(_f, "[FAILED] %s\n", test_name);
         fprintf(_f, "expected=\n");
         dump(expected, mem_size, 16);
         fprintf(_f, "\n");
@@ -61,7 +61,7 @@ void assert_by_str(char *expected,
     int cmp = strcmp(expected, result);
     int success = (cmp == 0);
     if (failed(success)) {
-        fprintf(_f, "failed %s\n", test_name);
+        fprintf(_f, "[FAILED] %s\n", test_name);
         fprintf(_f, "expected=\"%s\"\n  result=\"%s\"\n", expected, result);
     }
 }
@@ -71,7 +71,7 @@ void assert_by_null(void *result,
 {
     int success = result == NULL;
     if (failed(success)) {
-        fprintf(_f, "failed %s\n", test_name);
+        fprintf(_f, "[FAILED] %s\n", test_name);
         fprintf(_f, "result=%p is not NULL\n", result);
     }
 }
@@ -81,7 +81,7 @@ void assert_by_not_null(void *result,
 {
     int success = result != NULL;
     if (failed(success)) {
-        fprintf(_f, "failed %s\n", test_name);
+        fprintf(_f, "[FAILED] %s\n", test_name);
         fprintf(_f, "result=%p\n", result);
     }
 }
@@ -92,7 +92,7 @@ void assert_by_address(void *expected,
 {
     int success = expected == result;
     if (failed(success)) {
-        fprintf(_f, "failed %s\n", test_name);
+        fprintf(_f, "[FAILED] %s\n", test_name);
         fprintf(_f, "expected=%p, result=%p\n", expected, result);
     }
 }
@@ -103,7 +103,7 @@ void assert_by_size(size_t expected,
 {
     int success = expected == result;
     if (failed(success)) {
-        fprintf(_f, "failed %s\n", test_name);
+        fprintf(_f, "[FAILED] %s\n", test_name);
         fprintf(_f, "expected=%zu, result=%zu\n", expected, result);
     }
 }
@@ -125,14 +125,14 @@ void assert_by_ushort(ushort expected,
 void assert_success(int ret, char *test_name)
 {
     if (failed(ret == SUCCESS)) {
-        fprintf(_f, "failed %s\n", test_name);
+        fprintf(_f, "[FAILED] %s\n", test_name);
     }
 }
 
 void assert_true(int success, char *test_name)
 {
     if (failed(success)) {
-        fprintf(_f, "failed %s\n", test_name);
+        fprintf(_f, "[FAILED] %s\n", test_name);
     }
 }
 
