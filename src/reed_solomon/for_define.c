156207d35ea1105d22e276088be0d175a36803c4
umedoblock <devnull@localhost>
2014-02-21 22:14:03 +0900

    共通部分を MACRO 化してみたり。

--- src/reed_solomon/for_define.c
+++ src/reed_solomon/for_define.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-#define AAA (1)
-
 typedef unsigned int uint;
 typedef unsigned short ushort;
 
@@ -20,6 +18,30 @@ uint mul32(uint n, number_t *num)
     return n * num->u32;
 }
 
+#define _mulXXX(X) \
+    for (;;) { \
+        _TMP = n * num->u##X; \
+        break; \
+    }
+
+uint _mul16(uint n, number_t *num)
+{
+    ushort _TMP;
+
+    _mulXXX(16);
+
+    return _TMP;
+}
+
+uint _mul32(uint n, number_t *num)
+{
+    ushort _TMP;
+
+    _mulXXX(16);
+
+    return _TMP;
+}
+
 int main(void)
 {
     number_t num_, *num = &num_;
@@ -28,10 +50,12 @@ int main(void)
     n = 16;
     num->u16 = 16;
     fprintf(stdout, "mul16(n=16, 16) = %u\n", mul16(n, num));
+    fprintf(stdout, "_mul16(n=16, 16) = %u\n", _mul16(n, num));
 
     n = 32;
     num->u32 = 32;
     fprintf(stdout, "mul32(n=32, 32) = %u\n", mul32(n, num));
+    fprintf(stdout, "_mul32(n=32, 32) = %u\n", _mul32(n, num));
 
     return 0;
 }
