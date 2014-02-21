a2e5c57d88d3f76c9417424cc3a715ce6c6011e9
umedoblock <devnull@localhost>
2014-02-21 22:22:47 +0900

    union の内容を参照した時に、参照先の memory の型(=ushort or uint)が 違っていた場合、実行結果に違いが生じる様にした。

--- src/reed_solomon/for_define.c
+++ src/reed_solomon/for_define.c
@@ -35,9 +35,9 @@ uint _mul16(uint n, number_t *num)
 
 uint _mul32(uint n, number_t *num)
 {
-    ushort _TMP;
+    uint _TMP;
 
-    _mulXXX(16);
+    _mulXXX(32);
 
     return _TMP;
 }
@@ -47,15 +47,18 @@ int main(void)
     number_t num_, *num = &num_;
     uint n = 0;
 
-    n = 16;
-    num->u16 = 16;
+    n = 1;
+
+    num->u16 = 1;
     fprintf(stdout, "mul16(n=16, 16) = %u\n", mul16(n, num));
     fprintf(stdout, "_mul16(n=16, 16) = %u\n", _mul16(n, num));
 
-    n = 32;
-    num->u32 = 32;
+    num->u32 = 65536;
     fprintf(stdout, "mul32(n=32, 32) = %u\n", mul32(n, num));
     fprintf(stdout, "_mul32(n=32, 32) = %u\n", _mul32(n, num));
+
+    fprintf(stdout, "mul16(n=16, 16) = %u\n", mul16(n, num));
+    fprintf(stdout, "_mul16(n=16, 16) = %u\n", _mul16(n, num));
 
     return 0;
 }
