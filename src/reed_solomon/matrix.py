7ff950635caf7dd0538957a48ae55a3768b479f9
umedoblock <devnull@localhost>
2014-03-02 00:36:41 +0900

    逆行列計算できてないけど眠いので寝る。 寝る子は育つ！ (refs #338)

--- src/reed_solomon/matrix.py
+++ src/reed_solomon/matrix.py
@@ -1,10 +1,14 @@
 import collections
+import copy
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4, width=40)
 
 class Matrix(object):
     def __init__(self, seq):
+        if isinstance(seq, Matrix):
+            seq = seq._matrix
+
         if not isinstance(seq, collections.Sequence) or \
            not isinstance(seq[0], collections.Sequence):
             raise ValueError("seq and seq[0] must be Sequence object")
@@ -32,6 +36,7 @@ class Matrix(object):
     def __mul__(self, other):
         if self.len_columns != other.len_rows:
             raise ValueError("self.len_columns(={}) and other.len_rows(={}) must be equal.".format(self.len_columns, other.len_rows))
+
 #       print("self = {}".format(self))
 #       print("other = {}".format(other))
         mat = [None] * self.len_rows
@@ -58,7 +63,87 @@ class Matrix(object):
         print("mat = ")
         pp.pprint(mat)
 
-        return Matrix(mat)
+        return mat
+
+    # see in par2/par2/py3.py
+    def _make_square_matrix(self, value=None):
+        square_matrix = [None] * self.len_rows
+        for i in range(self.len_rows):
+            square_matrix[i] = [value] * self.len_rows
+        return square_matrix
+
+    # see in par2/par2/py3.py
+    def _make_e_matrix(self):
+        e = self._make_square_matrix(0)
+        for i in range(self.len_rows):
+            e[i][i] = 1
+        return e
+
+    # see in par2/par2/py3.py
+    def _solve_inverse_matrix(self):
+        if self.len_columns != self.len_rows:
+            raise ValueError("self.len_columns(={}) and self.len_rows(={}) must be equal.".format(self.len_columns, self.len_rows))
+        matrix = copy.deepcopy(self._matrix)
+        im = inverse_matrix = self._make_e_matrix()
+
+      # self._at_a_glance(matrix, im)
+      # print()
+        for k in range(self.len_rows):
+            # self._at_a_glance(matrix, im)
+            if not matrix[k][k]:
+                swap = False
+                for j in range(k + 1, self.len_rows):
+                    if matrix[j][k]:
+                        matrix[k], matrix[j] = matrix[j], matrix[k]
+                        im[k], im[j] = im[j], im[k]
+                        swap = True
+                        break
+                if not swap:
+                    message = 'cannot make inverse_matrix.'
+                    raise ValueError(message)
+
+            if matrix[k][k] != 1:
+                tmp = matrix[k][k]
+                for i in range(self.len_rows):
+                    matrix[k][i] = matrix[k][i] / tmp
+                    im[k][i] = im[k][i] / tmp
+
+            for j in range(k + 1, self.len_rows):
+                foo = matrix[j][k]
+                if not foo:
+                    continue
+                for i in range(self.len_rows):
+                    tmp1 = matrix[k][i]
+                    tmp2 = foo * tmp1
+                    tmp3 = matrix[j][i]
+                    matrix[j][i] = tmp3 + tmp2
+
+                    im1 = im[k][i]
+                    im2 = foo * im1
+                    im3 = im[j][i]
+                    im[j][i] = im3 + im2
+      # print('前進完了') # moving front done
+
+        for k in range(self.len_rows - 1):
+            for j in range(self.len_rows - 1 - k):
+                z = self.len_rows - 1 - k
+                x = self.len_rows - 1 - k
+                y = self.len_rows - 1 - k - j - 1
+                foo = matrix[y][x]
+
+                for i in range(self.len_rows):
+              #     tmp1 = matrix[z][i]
+              #     tmp2 = self._mul(foo, tmp1)
+              #     tmp3 = matrix[y][i]
+              #     matrix[y][i] = self._add(tmp3, tmp2)
+
+                    im1 = im[z][i]
+                    im2 = foo * im1
+                    im3 = im[y][i]
+                    im[y][i] = im3 + im2
+      # print()
+
+        return inverse_matrix
 
 def mul_matrixes(mat1, mat2):
     mat3 = [None] * 4
@@ -138,9 +223,9 @@ if __name__ == "__main__":
     print(mmat2)
     print("mmatE =")
     print(mmatE)
-    mmat = mmat1 * mmat2
-    print("mmat1 * mmat2 =")
-    print(mmat._matrix)
+#   mmat = mmat1 * mmat2
+#   print("mmat1 * mmat2 =")
+#   print(mmat._matrix)
     try:
         mmat1 * mmatE
     except ValueError as e:
@@ -154,3 +239,9 @@ if __name__ == "__main__":
     Matrix(mat2) * Matrix(matE)
     mmat3 * mmat2
     Matrix(mat1) * Matrix(mat2)
+    print("seqA ^ -1 =")
+    pp.pprint(Matrix(seqA)._solve_inverse_matrix())
+    pp.pprint(Matrix(seqA) * Matrix(Matrix(seqA)._solve_inverse_matrix()))
+    print("mat1 ^ -1 =")
+    pp.pprint(Matrix(mat1)._solve_inverse_matrix())
+    pp.pprint(Matrix(mat1) * Matrix(mat1)._solve_inverse_matrix())
