e143771e5a838e7b976b7e1ad022aabd4fd1d2af
umedoblock <devnull@localhost>
2015-03-11 21:59:45 +0900

    reed_solomon code を構成する poly を見つけるための python script.
    (refs #62)

---
+++ src/reed_solomon/galois.py
@@ -0,0 +1,40 @@
+# Copyright 2011 梅濁酒(umedoblock)
+import math
+import sys
+
+print(sys.argv)
+if len(sys.argv) == 1:
+    raise ValueError('you must use 1 argment')
+
+n_bits = int(sys.argv[1])
+poly = 1 << n_bits
+field_max = poly - 1
+
+digits = int(math.log(19, 10)) + 1
+fmt = '{:' + str(digits) + 'd}, 0b{:b}'
+# print('fmt =', fmt)
+print('n_bits =', n_bits)
+print('galois_field =')
+
+possible = set()
+for galois_field in range(1, poly, 2):
+# galois_field = 25
+    galois_field += poly
+    # set LSB
+    bit_pattern = 1
+    possible.clear()
+
+    for n in range(field_max):
+        if bit_pattern in possible:
+            break
+        possible.add(bit_pattern)
+        bit_pattern <<= 1
+        if bit_pattern & poly:
+            bit_pattern ^= galois_field
+
+    if len(possible) == field_max:
+        print(fmt.format(galois_field, galois_field))
+      # print('{:4d}, 0b{:b}'.format(galois_field, galois_field))
+      # print('possible =', possible)
+      # print()
+print()
